2e8b9252b9dccce9858a549387b76a57e336cf50
naraku0211 <kentivanunabia0211@gmail.com>
2024-02-16 14:53:54 +0800

    v1.0

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public ViewResult Index()
+    {
+        string display = "Hello First Project!!!"; 
+        return View("Index", display);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
